87088439da4b7104c38fdeac9820612d70572913
chathuranga jayamal <28252126+ChathurangaJayamal@users.noreply.github.com>
2020-05-24 12:55:41 +1200

    implementing...

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,4 +1,7 @@
 export { currentUserSaga } from "./currentUserSaga";
 export { fetchCartSaga } from "./fetchCartSaga";
 export { itemDetailsSaga } from "./itemDetailsSaga";
+export { itemQuantitySaga } from "./itemQuantitySaga";
+export { itemPriceSaga } from "./itemPriceSaga";
+
 // export { testTakeSaga, testPutSaga, testForkSaga } from "./testSaga";
